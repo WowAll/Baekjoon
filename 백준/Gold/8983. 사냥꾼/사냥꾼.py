3c40ac4350bae5df585c77e6b5d1409c302cb8f4
ChoMosigi <43941411+WowAll@users.noreply.github.com>
2025-09-11 15:36:50 +0900

    [Gold IV] Title: 사냥꾼, Time: 432 ms, Memory: 43448 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,30 +1,37 @@
+import sys
+
 def binary_search(tar):
     left = 0
     right = M - 1
-    shooter = left
     while left <= right:
         mid = (left + right) // 2
         if shooting[mid] < tar:
             left = mid + 1
         else:
             right = mid - 1
-            shooter = mid
-    return shooter
+    return left
 
 def can_shoot(n, animal_x, animal_y):
     range = abs(shooting[n] - animal_x) + animal_y
     return range <= L
 
-M, N, L = map(int, input().split())
+M, N, L = map(int, sys.stdin.readline().strip().split())
 
-shooting = list(map(int, input().split()))
+shooting = list(map(int, sys.stdin.readline().strip().split()))
 
 shooting.sort()
 
 ans = 0
 
 for i in range(N):
-    animal_x, animal_y = map(int, input().split())
-    if can_shoot(binary_search(animal_x), animal_x, animal_y):
+    animal_x, animal_y = map(int, sys.stdin.readline().strip().split())
+    if animal_y > L:
+        continue
+    idx = binary_search(animal_x)
+    if idx < M and can_shoot(idx, animal_x, animal_y):
         ans += 1
+        continue
+    if idx > 0 and can_shoot(idx - 1, animal_x, animal_y):
+        ans += 1
+
 print(ans)

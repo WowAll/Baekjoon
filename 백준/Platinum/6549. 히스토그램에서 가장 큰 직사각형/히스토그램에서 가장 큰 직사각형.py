819ce8474b79f25db880c959d45afc5f834d7ec9
ChoMosigi <43941411+WowAll@users.noreply.github.com>
2025-09-17 11:00:13 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 1076 ms, Memory: 47088 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -4,13 +4,16 @@ def cal_scale(arr, left, right):
     mid = left + (right - left) // 2
     left_cursor = mid - 1
     right_cursor = mid + 1
-    len = 1
+    cnt = 1
     res = arr[mid]
     unit = arr[mid]
+    len = right - left + 1
+    peak = unit * len
 
-    while True :
+    while res < peak :
         left_in_bound = left_cursor >= left
         right_in_bound = right_cursor <= right
+
         if not left_in_bound and not right_in_bound:
             return res
         if left_in_bound and (not right_in_bound or arr[left_cursor] >= arr[right_cursor]):
@@ -19,9 +22,12 @@ def cal_scale(arr, left, right):
         else:
             selected = arr[right_cursor]
             right_cursor += 1
-        len += 1
-        unit = min(selected, unit)
-        res = max(res, unit * len)
+        cnt += 1
+        if selected < unit:
+            unit = selected
+            peak = unit * len
+
+        res = max(res, unit * cnt)
     return res
 
 def histogram(arr, left, right):
@@ -38,5 +44,5 @@ def histogram(arr, left, right):
 N = list(map(int, sys.stdin.readline().strip().split()))
 
 while N[0] != 0:
-    print(histogram(N[1:], 0, len(N[1:]) - 1))
+    print(histogram(N[1:], 0, N[0] - 1))
     N = list(map(int, sys.stdin.readline().strip().split()))
